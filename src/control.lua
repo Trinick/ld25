c24d194af5b378248b326ce31b38e9cd615b1e76
Trinick <spambf@mac.com>
2012-12-14 23:58:06 -0800

    Preliminary movement control

---
+++ src/control.lua
@@ -0,0 +1,42 @@
+Control = {}
+Control.__index = Control
+
+function Control.new(world)
+	local inst = {}
+
+	setmetatable(inst, World)
+
+	inst.world = world;
+	inst.currcontrol = {}
+
+	return inst
+end
+
+function Control:check(dt)
+	for i, entity in pairs(self.currcontrol) do
+		if love.keyboard.isDown(w) then
+			ns = entity.movespeed * dt;
+		end
+		if love.keyboard.isDown(d) then
+			ew = entity.movespeed * dt;
+		end
+		if love.keyboard.isDown(s) then
+			ns = entity.movespeed * dt * -1;
+		end
+		if love.keyboard.isDown(a) then
+			ew = entity.movespeed * dt * -1;
+		end
+		entity.x += ns;
+		entity.y += ew;
+	end
+end
+
+function Control:onclick(x, y, button)
+	if button == 1 then
+		for i, entity in pairs(self.world.entities) do
+			if entity.collisionCheck(x, y) && entity.canControl then
+				this.currcontrol[1] = entity;
+			end
+		end
+	end
+end
--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,21 @@
 World = require "world"
+control = require "control"
 
 function love.load()
     love.graphics.setBackgroundColor(255, 255, 255)
 
     world = World.new()
+    control = Control.new(world);
 end
 
 function love.update(dt)
+	control:movecheck(dt)
 end
 
 function love.draw()
     world:render()
+end
+
+function love.mousepressed(x, y, button)
+	control:onclick(x, y, button)
 end
