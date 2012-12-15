78cee8e674142865e171a9c6d0ba97bd6ff539f4
Trinick <spambf@mac.com>
2012-12-14 23:46:51 -0800

    Fixing 7z

--- util/dist.bat
+++ util/dist.bat
@@ -1,7 +1,6 @@
-mkdir ../dist
-cd ../src
-7za a -tzip ..\dist\ld25.love *
+mkdir ..\dist
 cd ..
+deps\7za a -tzip dist\ld25.love src\*
 cd deps
 copy /b love.exe+..\dist\ld25.love ..\dist\ld25.exe
 copy /b *.dll ..\dist\
