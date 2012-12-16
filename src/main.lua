c5d8e18efe343bcb8998d85e6b1034613554074f
Johnny Patterson <trigun@centhra.com>
2012-12-15 18:01:58 -0800

    Added raycasting

--- src/main.lua
+++ src/main.lua
@@ -9,6 +9,46 @@ function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
 end
 function collision_stop(dt, shape_a, shape_b)
 end
+function raycast(startX, startY, endX, endY, ignoreSet)
+    local difX = endX - startX
+    local difY = endY - startY
+    local dist = math.sqrt(math.pow(difX, 2) + math.pow(difY, 2))
+    local inc = 16
+    local stepX = difX * inc / dist
+    local stepY = difY * inc / dist
+    local x = startX
+    local y = startY
+    local isFound = {}
+    local found = {}
+
+    if ignoreSet == nil then
+        ignoreSet = {}
+    end
+
+    for i = 0, dist, inc do
+        for _, shape in ipairs(Collider:shapesAt(x, y)) do
+            if ignoreSet[shape] == nil and isFound[shape] == nil then
+                isFound[shape] = 1
+                table.insert(found, shape)
+            end
+        end
+        x = x + stepX
+        y = y + stepY
+    end
+
+    if (x - startX) > difX or (y - startY) > difY then
+        x = endX
+        y = endX
+        for _, shape in ipairs(Collider:shapesAt(x, y)) do
+            if ignoreSet[shape] == nil and isFound[shape] == nil then
+                isFound[shape] = 1
+                table.insert(found, shape)
+            end
+        end
+    end
+
+    return found
+end
 
 function love.load()
     Collider = HC(100, on_collision, collision_stop)
