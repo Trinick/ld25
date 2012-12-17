b86bbdf31340288342c67fbb1b6fa906100c2785
Trinick <spambf@mac.com>
2012-12-16 23:34:57 -0800

    Fixed linux build shell

--- util/dist.sh
+++ util/dist.sh
@@ -1,5 +1,5 @@
 mkdir -p dist
 cd src && zip -r9 ../dist/ld25.love * && cd ..
 zip -r9 dist/ld25.love art
-cat `which love` dist/ld25.love > dist/ld25 && chmod +x dist/phaedra
+cat `which love` dist/ld25.love > dist/phaedra && chmod +x dist/phaedra
 dist/phaedra
