544e5df4f98e57153fd534cbdd5c80f4247b3bcf
Zack Corr <zack@z0w0.me>
2012-12-17 17:42:33 +1000

    Rename .love files

--- util/dist.bat
+++ util/dist.bat
@@ -1,9 +1,9 @@
 mkdir ..\dist
 cd ..\src
-..\deps\7za a -tzip ..\dist\ld25.love * ..\art
+..\deps\7za a -tzip ..\dist\phaedra.love * ..\art
 cd ..
 cd deps
-copy /b love.exe+..\dist\ld25.love ..\dist\phaedra.exe
+copy /b love.exe+..\dist\phaedra.love ..\dist\phaedra.exe
 copy /b *.dll ..\dist\
 cd ..
 dist\phaedra.exe
